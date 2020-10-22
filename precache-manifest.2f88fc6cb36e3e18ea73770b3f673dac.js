7492f60af3bd034a0dc334b323205ab2c5430663
Haaitham <hythamdefrawy@gmail.com>
2020-10-22 12:36:55 -0400

    Updates

---
+++ precache-manifest.2f88fc6cb36e3e18ea73770b3f673dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b2cbe008cb789a3899f866ea14796c",
+    "url": "/Robo-Monsters/index.html"
+  },
+  {
+    "revision": "e24b1a015a2d7b15f5fb",
+    "url": "/Robo-Monsters/static/css/main.ccc30c38.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/Robo-Monsters/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Robo-Monsters/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24b1a015a2d7b15f5fb",
+    "url": "/Robo-Monsters/static/js/main.41be80c9.chunk.js"
+  },
+  {
+    "revision": "7919caa7a80072c4ff04",
+    "url": "/Robo-Monsters/static/js/runtime-main.443150bf.js"
+  }
+]);
